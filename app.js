09a56363a285893e7e00e8787f64a8d7e66b823d
michael-valenti <48532713+michael-valenti@users.noreply.github.com>
2022-03-12 17:03:32 -0500

    When get function is trigged for root/homepage, pass the posts array to it.

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const ejs = require("ejs");
 
 const port = 3000;
 
-const posts =[];
+let posts =[];
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -23,12 +23,8 @@ app.use(express.static("public"));
 
 app.get("/", function(req, res){
 
-//render the home page and pass homeStartingContent to it
-res.render("home", {homeStartingContent});
-
-for (i = 0; i < posts.length; i++){
-  console.log(posts[i]);
-}
+//render the home page and pass homeStartingContent and posts array to it
+res.render("home", {homeStartingContent, posts});
 
 });
 
@@ -54,10 +50,12 @@ res.render("compose");
 });
 
 app.post("/", function(req, res){
+  //store title and body inside of an object
   const post = {
     title: req.body.postTitle,
     post: req.body.postBody
   };
+  //push the object to an array named "posts and then redirect to the homepage"
   posts.push(post);
   res.redirect("/");
 
