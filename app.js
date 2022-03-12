897671f8065dfb8a3395ce4962e03e3c181e8332
michael-valenti <48532713+michael-valenti@users.noreply.github.com>
2022-03-12 16:24:17 -0500

    When a post is published, store the title and body inside an object. Then push the object to an array. Redirect to homepage once this happens.

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const ejs = require("ejs");
 
 const port = 3000;
 
+const posts =[];
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -24,6 +25,10 @@ app.get("/", function(req, res){
 
 //render the home page and pass homeStartingContent to it
 res.render("home", {homeStartingContent});
+
+for (i = 0; i < posts.length; i++){
+  console.log(posts[i]);
+}
 
 });
 
@@ -49,8 +54,12 @@ res.render("compose");
 });
 
 app.post("/", function(req, res){
-console.log(req.body.postTitle);
-console.log(req.body.postBody);
+  const post = {
+    title: req.body.postTitle,
+    post: req.body.postBody
+  };
+  posts.push(post);
+  res.redirect("/");
 
 });
 
